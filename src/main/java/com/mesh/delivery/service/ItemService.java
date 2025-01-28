80360e28d6e082cf6c67e48f246b641b61c45b87
meshach5667 <meshachzakumi@gmail.com>
2025-01-28 13:34:30 +0100

    Resolved merge conflicts

--- src/main/java/com/mesh/delivery/service/ItemService.java
+++ src/main/java/com/mesh/delivery/service/ItemService.java
@@ -3,12 +3,6 @@ package com.mesh.delivery.service;
 import com.mesh.delivery.Entity.Item;
 
 public interface ItemService {
-    static Item getItemById(Long itemId) {
-        return null;
-    }
-
-    Item createItem();
-
     Item createItem(Item item);
 
     void deleteItem(Long id);
